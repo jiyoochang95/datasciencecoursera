b786827c8d36a04b0857ecd55ae8eecfadb2e895
Jiyoo Chang <jiyoochang95@gmail.com>
2017-08-25 16:56:50 -0700

    Create best.R

---
+++ rprogramming/lab2/best.R
@@ -0,0 +1,9 @@
+best<-fuction(state,outcome){
+    ##Read outcome data
+    
+    ## Check that state and outcome are valid
+    
+    ## Return hospital name in that state with lowest 30-day rate
+    
+    
+}
